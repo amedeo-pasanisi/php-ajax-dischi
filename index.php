ae51031428914c66f623042b99a4de331435f1a9
amedeo <amedeopasanisi97@gmail.com>
2021-10-12 15:11:33 +0200

    immagini, style, fine milestone1

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset='UTF-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>Dischi</title>
 </head>
 <body>
@@ -15,10 +19,10 @@
             <?php
                 foreach ($db as $disco) {
                     echo '<div class="disco">';
-                        // echo '<img :src= "disco.poster" alt="">';
-                        echo '<h3>' . $disco["title"] . '</h3>';
-                        echo '<div>' . $disco["author"] . '</div>';
-                        echo '<div>' . $disco["year"] . '</div>';
+                        echo '<img src=' . $disco['poster'] . '>';
+                        echo '<h3>' . $disco['title'] . '</h3>';
+                        echo '<div>' . $disco['author'] . '</div>';
+                        echo '<div>' . $disco['year'] . '</div>';
                     echo '</div>';
                 }
             ?>
