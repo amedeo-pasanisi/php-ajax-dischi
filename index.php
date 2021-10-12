f438a78de72aa1eae1f1158ee5263920e00e7fa4
amedeo <amedeopasanisi97@gmail.com>
2021-10-12 14:49:30 +0200

    stampa php testo

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang='en'>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Dischi</title>
 </head>
 <body>
     <?php
-    include __DIR__ . "/database.php";
+    include __DIR__ . '/database.php';
     ?>
+    <main>
+        <div class='container'>
+            <?php
+                foreach ($db as $disco) {
+                    echo '<div class="disco">';
+                        // echo '<img :src= "disco.poster" alt="">';
+                        echo '<h3>' . $disco["title"] . '</h3>';
+                        echo '<div>' . $disco["author"] . '</div>';
+                        echo '<div>' . $disco["year"] . '</div>';
+                    echo '</div>';
+                }
+            ?>
+        </div>
+    </main>
 </body>
 </html>
+<?php
+
+?>
